292fa97b62f8a9867747fbca2f482ff2256a1e57
gettosan <gettoojjk@gmail.com>
2025-04-18 11:36:44 +0530

    Update Poll Account in initialize_candidate Function

--- anchor/tests/voting.spec.ts
+++ anchor/tests/voting.spec.ts
@@ -69,6 +69,14 @@ describe("Voting", () => {
     console.log(blueCandidate);
     expect(blueCandidate.candidateVotes.toNumber()).toBe(0);
     expect(blueCandidate.candidateName).toBe("Blue");
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, "le", 8)],
+      votingProgram.programId,
+    )
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+    console.log(poll);
+    expect(poll.candidateAmount.toNumber()).toBe(2);
   });
 
   it("vote candidates", async () => {
